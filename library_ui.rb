eaf48b8e83d84382bd8de1a0282e6750bc9f11e9
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-12 07:53:53 -0700

    allow users to add titles-UI

---
+++ library_ui.rb
@@ -0,0 +1,58 @@
+require 'pg'
+require './lib/title'
+require './lib/author'
+require './lib/publisher'
+
+DB = PG.connect({:dbname => 'library'})
+
+def main_menu
+	puts "Press 1 to add a title"
+	puts "Press 2 view all titles"
+	puts "Press 3 to add an author"
+	puts "Press 4 to view all authors"
+	puts "Press 5 to add a publisher"
+	puts "Press 6 to view all publishers"
+	puts "Press 7 to search"
+	puts "Press 8 to exit"
+	choice = gets.chomp.to_i
+	if choice == 1
+		add_title
+		main_menu
+	elsif choice == 2
+		view_titles
+		main_menu
+	elsif choice == 3
+		add_author
+		main_menu
+	elsif choice == 4
+		view_authors
+		main_menu
+	elsif choice == 5
+		add_publisher
+		main_menu
+	elsif choice == 6
+		view_publishers
+		main_menu
+	elsif choice == 7
+		search
+		main_menu
+	elsif choice = 8
+		puts "Bye!"
+		exit
+	end
+end
+
+def add_title
+	puts "Enter the book's title:"
+	title = gets.chomp
+	new_title = Title.new("name" => title)
+	new_title.save
+	puts "Title added!"
+end
+
+main_menu
+			
+			
+			
+	
+
