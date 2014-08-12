c4f73aa6021fd6f06bc681673cd1349311ac82d5
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-11 19:02:32 -0700

    list all authors from the DB

--- lib/author.rb
+++ lib/author.rb
@@ -5,4 +5,14 @@ class Author
 		@name = author_info['name']
 		@id = author_info['id'].to_i
 	end
+
+	def self.all
+		authors = []
+    results = DB.exec("SELECT * FROM author;")
+    results.each do |result|
+      new_author = Author.new(result)
+      authors << new_author
+    end
+    authors
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,10 +1,17 @@
 require "spec_helper"
 
 describe "Author" do
+
 	describe "initialize" do
 		it "initializes with a name" do
 			new_author = Author.new("Dr.Suess")
 			expect(new_author).to be_an_instance_of Author
+		end
+	end
+
+	describe "self.all" do
+		it 'pulls all authors out the DB and saves them as objects' do
+			expect(Author.all).to eq []
 		end
 	end
 end
