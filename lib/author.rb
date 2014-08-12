b4d01e1498a73f42c695e9988997dc48c84e0321
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-11 19:11:55 -0700

    save new authors to the DB

--- lib/author.rb
+++ lib/author.rb
@@ -2,8 +2,8 @@ class Author
 	attr_reader :name, :id
 
 	def initialize(author_info)
-		@name = author_info['name']
-		@id = author_info['id'].to_i
+		@name = author_info["name"]
+		@id = author_info["id"].to_i
 	end
 
 	def self.all
@@ -15,4 +15,13 @@ class Author
     end
     authors
   end
+
+  def save
+		result = DB.exec("INSERT INTO author (name) VALUES ('#{name}') RETURNING id;")
+		@id = result.first['id'].to_i
+	end
+
+	def ==(author_two)
+		self.name == author_two.name
+	end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,6 +14,23 @@ describe "Author" do
 			expect(Author.all).to eq []
 		end
 	end
+
+	describe "save" do
+		it 'saves new authors in the DB' do
+			new_author = Author.new({"name" => "Dr.Suess"})
+			new_author.save
+			expect(Author.all).to eq [new_author]
+		end
+	end
+	
+	describe "==" do
+  	it "makes two objects equal to one another if they have the same author" do
+  		new_author = Author.new({"name" => "Dr.Suess"})
+  		new_author_two = Author.new({"name" => "Dr.Suess"})
+  		expect(new_author).to eq new_author_two
+  	end
+  end
+
 end
 
 
